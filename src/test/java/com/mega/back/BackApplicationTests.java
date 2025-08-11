870da1933345b897feb106621187106fec21bbb7
Alejandro Flores <alejandro.flores.a@ucb.edu.bo>
2025-08-11 16:41:08 -0400

    first commit

---
+++ src/test/java/com/mega/back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mega.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
